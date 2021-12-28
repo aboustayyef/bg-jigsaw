4fc8bf4772cc93eab73dec830cba2810e099ed0b
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2021-12-28 11:11:53 +0000

    Main Init

---
+++ source/_assets/js/main.js
@@ -0,0 +1,4 @@
+// Alpine JS
+import Alpine from 'alpinejs'
+window.Alpine = Alpine
+Alpine.start()
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    'source/**/*.blade.php',
+    'source/**/*.md',
+    'source/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
