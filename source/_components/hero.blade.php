937ce97383993214ad3556a3058a2928a82b8a26
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2021-12-29 15:47:52 +0000

    slide show now working

--- source/_components/hero.blade.php
+++ source/_components/hero.blade.php
@@ -1,14 +1,24 @@
+@php
+    $all_slides = ["laptops-and-couch.jpg", "reference_silver_088.jpg", "miro-2.jpg", "morrison_00.jpg", "milieu_chefbuero.jpg", "interstuhl_ambient_yellow.jpg", "vista_74_75.jpg", "sanders_Letto_001.jpg", "reference_airpad_01.jpg"];
+    $slides_to_show = array_rand($all_slides, 4);
+@endphp
+
 <section>
-  <div class="container max-w-7xl mx-auto bg-gray-100 relative overflow-hidden" >
-    <div class="main-carousel mx-auto" data-flickity='{ "autoPlay":true, "contain": true, "bgLazyLoad": true, "imagesLoaded": true, "pageDots": true }'>
-      <div class="carousel-cell w-full aspect-video object-cover bg-cover" data-flickity-bg-lazyload="https://source.unsplash.com/random/1200x500/?sig=1" ></div>
-      <div class="carousel-cell w-full aspect-video object-cover bg-cover" data-flickity-bg-lazyload="https://source.unsplash.com/random/1280x500/?sig=3"></div>
-      <div class="carousel-cell w-full aspect-video object-cover bg-cover" data-flickity-bg-lazyload="https://source.unsplash.com/random/1300x500/?sig=5"></div>
-    
+  <div class="container w-full max-w-7xl mx-auto bg-gray-100 relative" >
+
+    {{-- slides --}}
+    <div class="main-carousel w-full" data-flickity='{ "fade":true, "autoPlay":true, "contain": true, "bgLazyLoad": true, "imagesLoaded": true, "pageDots": false }'>
+      <div class="carousel-cell w-full aspect-video bg-cover" data-flickity-bg-lazyload="/assets/images/slides/lennox_soft_2016_002.jpg" ></div>
+      @foreach($slides_to_show as $slide)
+      <div class="carousel-cell w-full aspect-video bg-cover" data-flickity-bg-lazyload="{{'/assets/images/slides/'. $all_slides[$slide]}}" ></div>
+      @endforeach
     </div>
+
+    {{-- Call to action --}}
     <div class="absolute top-8 left-8">
       <h2 class="mb-4 text-2xl font-bold z-10">Call to Action on Top of slideshow</h2>
       <a class= "linear-block bg-green-800 px-4 py-2 hover:bg-green-900 text-white rounded" href="#">Take Action</a>
     </div>
+  
   </div>
 </section>
